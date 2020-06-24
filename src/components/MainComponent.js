031f36a6f496d7b88e3db910fba89c3473ad5f08
chaitrali-shinde <chai.shinde100@gmail.com>
2020-06-24 13:57:49 +0530

    homepg updated

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Dishdetail from './DishdetailComponent'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
@@ -14,6 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
      
     };
   }
@@ -23,7 +29,10 @@ class Main extends Component {
 
     const HomePage=()=>{
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
+              leader={this.state.leaders.filter((leader)=> leader.featured)[0]}
+        />
       )
     }
     return (
