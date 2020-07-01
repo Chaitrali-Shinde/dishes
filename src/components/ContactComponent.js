873d9b99a9df74f7c7130f1fd2aecf5d9a3e082d
chaitrali-shinde <chai.shinde100@gmail.com>
2020-07-01 14:17:47 +0530

    reactRedux Form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,82 +1,26 @@
 import React from 'react';
 import {Breadcrumb, BreadcrumbItem, Input, Form, FormGroup, Col, Button, Label, FormFeedback} from 'reactstrap';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import {Control, Errors, LocalForm} from 'react-redux-form'
 
 class Contact extends React.Component {
 
     constructor(props){
         super(props);
-        this.state= {
-            firstname: "",
-            lastname: "",
-            email: "",
-            telnum: "",
-            agree: false,
-            contactType: "Tel.",
-            message: "", 
-            touched:{
-                firstname: false,
-                lastname: false,
-                telnum: false,
-                email: false
-            }
-        }
-        this.handleInputChange= this.handleInputChange.bind(this);
+        
         this.handleSubmit= this.handleSubmit.bind(this);
-        this.handleBlur= this.handleBlur.bind(this);
-    }
-    handleInputChange(e){
-        const target= e.target;
-        const value= target.type=== 'checkbox'? target.checked: target.value;
-        const name= target.name;        
-        this.setState({
-            [name]: value
-        })
-
     }
 
-    handleSubmit(e){
-        console.log("Current state is:"+ JSON.stringify(this.state));
-        alert("Current state is:"+ JSON.stringify(this.state));
-        e.preventDefault();
+
+    handleSubmit(values){
+        console.log("Current state is:"+ JSON.stringify(values));
+        alert("Current state is:"+ JSON.stringify(values));
     }
 
-    handleBlur= (field)=>(evt)=>{
-        this.setState({
-                touched: {...this.state.touched, [field]: true}
-        })
-    }
 
-    validate(firstname, lastname, email, telnum){
-        const errors={
-            firstname: "",
-            lastname: "",
-            email: "",
-            telnum: "",
-        }
-        if(this.state.touched.firstname && firstname.length<3){
-            errors.firstname= 'First Name cannot be so short';}
-        else if(this.state.touched.firstname && firstname.length>10){
-            errors.firstname= 'First Name cannot be so long';
-        }
-
-        if(this.state.touched.lastname && lastname.length<3){
-            errors.lastname= 'Last Name cannot be so short';}
-        else if(this.state.touched.lastname && lastname.length>10){
-            errors.lastname= 'Last Name cannot be so long';
-        }
-
-        const reg= /^\d+$/;
-        if(this.state.touched.telnum&& !reg.test(telnum)){
-            errors.telnum= 'Invalid telephone number';
-        }
-        if(this.state.touched.email&& email.split('').filter(x=> x==='@').length!=1){
-            errors.email= 'Email invalid'
-        }
-        return errors;
-    }
+   
     render(){
-        const errors= this.validate(this.state.firstname, this.state.lastname, this.state.email, this.state.telnum)
+      
         return(
             <div className="container">
                 <div className="row">
@@ -122,20 +66,17 @@ class Contact extends React.Component {
                         <h3>Send us your feedback</h3>
                     </div>
                     <div className= "col-12 col-md-9">
-                        <Form onSubmit={this.handleSubmit}>
+                        <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
                             <FormGroup row>
                                 <Label htmlFor= "firstname" md= {2}>
                                     First Name
                                 </Label>
                                 <Col md={10}>
-                                    <Input type= "text" id= "firstname" name= "firstname"
+                                    <Control.text model= ".firstname" id= "firstname" name= "firstname"
                                             placeholder= "First Name" 
-                                            value= {this.state.firstname}
-                                            valid= {errors.firstname=== ''}
-                                            invalid= {errors.firstname != ''}
-                                            onBlur= {this.handleBlur('firstname')}
-                                            onChange={this.handleInputChange}/>
-                                    <FormFeedback>{errors.firstname}</FormFeedback>
+                                            className= "form-control"
+                                            />
+                                   
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -143,14 +84,11 @@ class Contact extends React.Component {
                                     Last Name
                                 </Label>
                                 <Col md={10}>
-                                    <Input type= "text" id= "lastname" name= "lastname"
+                                    <Control.text model= ".lastname" id= "lastname" name= "lastname"
                                             placeholder= "Last Name" 
-                                            value= {this.state.lastname}
-                                            valid= {errors.lastname=== ''}
-                                            invalid= {errors.lastname != ''}
-                                            onBlur= {this.handleBlur('lastname')}
-                                            onChange={this.handleInputChange}/>
-                                    <FormFeedback>{errors.lastname}</FormFeedback>
+                                            className= "form-control"
+                                            />
+                                    
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -158,14 +96,11 @@ class Contact extends React.Component {
                                     Last Name
                                 </Label>
                                 <Col md={10}>
-                                    <Input type= "tel" id= "telnum" name= "telnum"
+                                    <Control.text model= ".telnum" id= "telnum" name= "telnum"
                                             placeholder= "Telephone No." 
-                                            value= {this.state.telnum}
-                                            valid= {errors.telnum=== ''}
-                                            invalid= {errors.telnum != ''}
-                                            onBlur= {this.handleBlur('telnum')}
-                                            onChange={this.handleInputChange}/>
-                                     <FormFeedback>{errors.telnum}</FormFeedback>
+                                            className= "form-control"
+                                            />
+                                   
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -173,32 +108,29 @@ class Contact extends React.Component {
                                     Email
                                 </Label>
                                 <Col md={10}>
-                                    <Input type= "email" id= "email" name= "email"
+                                    <Control.text model= ".email" id= "email" name= "email"
                                             placeholder= "Email" 
-                                            value= {this.state.email}
-                                            valid= {errors.email=== ''}
-                                            invalid= {errors.email != ''}
-                                            onBlur= {this.handleBlur('email')}
-                                            onChange={this.handleInputChange}/>
-                                    <FormFeedback>{errors.email}</FormFeedback>
+                                            className= "form-control"
+                                           />
+                                  
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
                                 <Col md= {{size:6, offset:2}}>
-                                    <FormGroup check>
+                                    <div className= "form-check">
                                         <Label check>
-                                            <Input type= "checkbox" name= "agree"
-                                                    checked= {this.state.agree}
-                                                    onChange={this.handleInputChange}/>{''}
+                                            <Control.checkbox model= ".agree" name= "agree"
+                                                className= "form-check-input"/>{''}
                                             <strong>Can we contact you?</strong>
                                         </Label>
-                                    </FormGroup>
+                                    </div>
                                 </Col>
                                 <Col md={{size:3, offset: 1}}>
-                                    <Input type="select" name= "contactType" value= {this.state.contactType}  onChange={this.handleInputChange}>
+                                    <Control.select model=".contactType" name= "contactType"
+                                                    className= "form-control">
                                         <option>Tel.</option>
                                         <option>Email</option>
-                                    </Input>
+                                    </Control.select>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -206,10 +138,9 @@ class Contact extends React.Component {
                                     Feedback
                                 </Label>
                                 <Col md={10}>
-                                    <Input type= "textarea" id= "feedback" name= "message"
+                                    <Control.textarea model= ".message" id= "feedback" name= "message"
                                             placeholder= "Feedback" 
-                                            value= {this.state.message}
-                                            onChange={this.handleInputChange}/>
+                                            className="form-control"/>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -217,7 +148,7 @@ class Contact extends React.Component {
                                     <Button type= "submit" color= "primary">Send Feedback</Button>
                                 </Col>
                             </FormGroup>
-                        </Form>
+                        </LocalForm>
                     </div>
                 </div>
             </div>
